36d4ad8c0462349304dd5a69fb70390b73828610
Nishibayashi Takuji <takuji31@gmail.com>
2012-08-17 13:18:39 +0900

    設定ファイルのパスをconfigにまとめる

--- config/test.pl
+++ config/test.pl
@@ -1,19 +1,6 @@
 use File::Spec;
 use File::Basename qw(dirname);
 my $basedir = File::Spec->rel2abs(File::Spec->catdir(dirname(__FILE__), '..'));
-my $dbpath;
-if ( -d '/home/dotcloud/') {
-    $dbpath = "/home/dotcloud/test.db";
-} else {
-    $dbpath = File::Spec->catfile($basedir, 'db', 'test.db');
-}
 +{
-    'DBI' => [
-        "dbi:SQLite:dbname=$dbpath",
-        '',
-        '',
-        +{
-            sqlite_unicode => 1,
-        }
-    ],
+    server_config_path => File::Spec->catfile($basedir, '/tmp.yaml'),
 };
